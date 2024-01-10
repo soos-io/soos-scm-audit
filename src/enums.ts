324d41298d0541790cfea76ba16693149d8acf5f
SOOS-JAlvarez <jalvarez@soos.io>
2024-01-10 15:16:33 -0300

    mvp done

--- src/enums.ts
+++
@@ -1,7 +0,0 @@
-export enum SCMSystem {
-  GitHub = "GitHub",
-  GitLab = "GitLab",
-  Bitbucket = "Bitbucket",
-  AzureDevOps = "AzureDevOps",
-  Other = "Other",
-}
--- src/index.ts
+++ src/index.ts
@@ -1,93 +1,44 @@
 #!/usr/bin/env node
-import { ArgumentParser } from "argparse";
-import { LogLevel, soosLogger } from "@soos-io/api-client";
-import { SCMSystem } from "./enums";
+import { version } from "../package.json";
+import { ScanType, ScmType, soosLogger } from "@soos-io/api-client";
 import { exit } from "process";
 import {
   obfuscateProperties,
-  sleep,
 } from "@soos-io/api-client/dist/utilities";
-import GitHubService from "./services/GitHubService";
+import ContributingDeveloperAuditService from "@soos-io/api-client/dist/services/ContributingDeveloperAuditService/ContributingDeveloperAuditService";
+import AnalysisArgumentParser, { ICommonArguments } from "@soos-io/api-client/dist/services/AnalysisArgumentParser";
 
-interface SOOSSCMAuditArgs {
-  clientId: string;
-  apiKey: string;
-  scmSystem: SCMSystem;
+interface SOOSSCMAuditArgs extends ICommonArguments {
   githubPAT: string;
-  logLevel: LogLevel;
-  verbose: boolean;
+  saveResults: boolean;
+  scmType: ScmType;
 }
 
 class SOOSSCMAudit { 
   constructor(private args: SOOSSCMAuditArgs) {}
 
   static parseArgs(): SOOSSCMAuditArgs {
-    const parser = new ArgumentParser({ description: `SOOS SCM Audit` });
+    const analysisArgumentParser = AnalysisArgumentParser.create(ScanType.SCM);
+    analysisArgumentParser.addSCMAuditArguments(
+      version,
+    );
 
-    parser.add_argument("--apiKey", {
-      help: `SOOS API Key`,
-      required: false,
-    });
-
-    parser.add_argument("--clientId", {
-      help: `SOOS Client ID`,
-      required: false,
-    });
-
-    parser.add_argument("--scmSystem", {
-      help: `SCM System`,
-      required: false,
-    });
-
-    parser.add_argument("--githubPAT", {
-      help: `GitHub Personal Access Token`,
-      required: false,
-    });
-
-    parser.add_argument("--logLevel", {
-      help: `Log Level`,
-      required: false,
-    });
-
-    parser.add_argument("--verbose", {
-      help: `Verbose`,
-      required: false,
-    });
-
-    const args = parser.parse_args();
-
-    return args;
+    soosLogger.info("Parsing arguments");
+    return analysisArgumentParser.parseArguments();
   }
 
   async runAudit(): Promise<void> {
-    const githubService = GitHubService.create(this.args.githubPAT);
-    soosLogger.info(`Fetching GitHub orgs`);
-    const orgs = await githubService.getGithubOrgs();
-    soosLogger.info(`Fetched GitHub orgs: ${JSON.stringify(orgs, null, 2)}`);
-  
-    // Await the resolution of all promises returned by the map
-    const repos = await Promise.all(orgs.map(async (org) => {
-      soosLogger.info(`Fetching GitHub org repos for ${org.login}`);
-      await sleep(1000); // Ensure sleep is awaited
-      const repos = await githubService.getGithubOrgRepos(org);
-      soosLogger.info(`Fetched GitHub org repos for ${org.login}: ${JSON.stringify(repos, null, 2)}`);
-      return repos;
-    }));
-  
-    soosLogger.info(`Fetched GitHub repos: ${JSON.stringify(repos, null, 2)}`);
-  
-    const contributors = await Promise.all(repos.flatMap(repoArray => 
-      repoArray.map(async (repo) => {
-        soosLogger.info(`Fetching GitHub repo contributors for ${repo.full_name}`);
-        await sleep(1000); // Ensure sleep is awaited
-        const contributors = await githubService.getContributorsForRepo(repo);
-        soosLogger.info(`Fetched GitHub repo contributors for ${repo.full_name}: ${JSON.stringify(contributors, null, 2)}`);
-        return contributors;
-      })
-    ));
+    const contributingDeveloperService = new ContributingDeveloperAuditService(ScmType.GitHub)
+    const auditParams = {
+      "githubPAT": this.args.githubPAT,
+    };
+    soosLogger.info(`Running Contributing Developer audit for ${this.args.scmType}`)
+    const contributingDevelopers = await contributingDeveloperService.audit(auditParams)
+    soosLogger.info(`Contributing Developers found: ${JSON.stringify(contributingDevelopers, null, 2)}`)
 
-    soosLogger.info(`Fetched GitHub contributors: ${JSON.stringify(contributors, null, 2)}`);
-  
+    if (this.args.saveResults) {
+      contributingDeveloperService.saveResults(contributingDevelopers)
+    }
   }
 
   static async createAndRun(): Promise<void> {
@@ -100,7 +51,7 @@ class SOOSSCMAudit {
       soosLogger.info("Configuration read");
       soosLogger.verboseDebug(
         JSON.stringify(
-          obfuscateProperties(args as unknown as Record<string, unknown>, ["apiKey"]),
+          obfuscateProperties(args as unknown as Record<string, unknown>, ["apiKey", "githubPAT"]),
           null,
           2,
         ),
